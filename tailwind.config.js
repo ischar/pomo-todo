ea6475f56e537f11ceec8a8fcc30e12db9b24bda
Jaeho Cha <ischar@naver.com>
2024-11-05 15:18:52 +0900

    chore(tailwind): add custom colors in tailwind.config.js
    - Tailwind 커스텀 컬러 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-bg-white": "#F5F5F5",
+        "light-bg-gray": {
+          100: "#ECECEC",
+          200: "#D9D9D9",
+        },
+        "light-bg-red": {
+          100: "#FD867B",
+          200: "#FF6F61",
+        },
+        "light-fg-black": "#343434",
+        "light-fg-gray": "#757575",
+        "light-fg-red": "#FF6F61",
+        
+        "dark-bg-black": "#121212",
+        "dark-bg-gray": {
+          100: "#333333",
+          200: "#444444",
+        },
+        "dark-bg-red": "#E55A50",
+        "dark-fg-white": "#EAEAEA",
+        "dark-fg-gray": "#CFCFCF",
+        "dark-fg-red": "#E55A50",
+      },
+    },
   },
   plugins: [],
-}
-
+};
